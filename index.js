ec8dac5652096ffe2bdb750cf487e640d30eaf21
thenataliam <102542373+thenataliam@users.noreply.github.com>
2023-05-30 23:59:01 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,20 +2,24 @@ const express = require("express"); //importar express
 const bodyParser = require("body-parser");
 const app = express(); //crear al servidor
 const port = process.env.PORT || 3000;
+const lista=[];
 app.use(bodyParser.urlencoded({ extended:false}));
 app.use(bodyParser.json());
 app.listen(port, () => {
  console.log(`hola servidor ejecucion en http://localhost:${port}`);
 })
 
-app.post('/usuario', (req, res)=> {
-    console.log('Cuerpo de la peticion: ', req.body);
-    const {nombre, correo, telefono, mensaje}=req.body;
-    console.log(nombre);
-    console.log(correo);
-    console.log(telefono);
-    console.log(mensaje);
+app.get('/getUsuarios', (req, res) => {
+ res.json(lista);
+});
 
-    res.send({message:"El usuario se ha recibido"});
-})
+app.post('/usuario', (req, res)=> {
+    const list=req.body.user;
+ lista.push(list);
+ res.json("usuario añadido);
+});
+
+ app.get('/', (req, res) => {
+  res.send('La api funciona');
+ });
 module.exports = app;
